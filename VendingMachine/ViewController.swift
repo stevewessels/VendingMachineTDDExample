fc63ace07232230bfd073d03ff8a7369402f6f38
Stephan Wessels <stevewessels@me.com>
2017-03-05 15:25:39 -0600

    Initial Commit

---
+++ VendingMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by Stephan B Wessels on 3/5/17.
+//  Copyright © 2017 Plum Street Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
